66eb37c7843ce8c97c4db14ff6bad8bd8ac313a8
joannagiga <giga.joanna123@gmail.com>
2023-10-18 09:04:53 +0800

    commit update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
@@ -21,8 +21,16 @@ def create_app(test_config=None):
     mysql.init_app(app)
     CSRFProtect(app)
 
-    from .user import user_bp as user_blueprint
-    app.register_blueprint(user_blueprint)
-
-
+    @app.route('/')
+    def students():
+        return render_template('students.html')
+    
+    @app.route('/course/')
+    def course():
+        return render_template('course.html')
+    
+    @app.route('/college/')
+    def college():
+        return render_template('college.html')
+   
     return app
